86b33812de8f3a1a2fad6922ed10d551957430ff
Matt Crouch <matt@mattcrouch.net>
2019-01-28 23:32:44 +0000

    Serve from Github subdirectory

---
+++ gradientpicker/precache-manifest.24c1b6b88c179ed6f407e421b1c0eb8c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "05685f7304f23069dc7e",
+    "url": "/gradientpicker/static/js/runtime~main.05685f73.js"
+  },
+  {
+    "revision": "82d307646c91131c82da",
+    "url": "/gradientpicker/static/js/main.82d30764.chunk.js"
+  },
+  {
+    "revision": "d30ebab64d3eb362c511",
+    "url": "/gradientpicker/static/js/1.d30ebab6.chunk.js"
+  },
+  {
+    "revision": "8f0ead2bf84b35ccd7655b7bb939ed3b",
+    "url": "/gradientpicker/index.html"
+  }
+];
--- gradientpicker/service-worker.js
+++ gradientpicker/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.236d71a1fd7c580f27d795d4be66c057.js"
+  "/gradientpicker/precache-manifest.24c1b6b88c179ed6f407e421b1c0eb8c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gradientpicker/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
