92c280431386de482da17a16ec93926b37c5efc5
Matt Crouch <matt@mattcrouch.net>
2020-01-14 21:55:14 +0000

    Victory example

---
+++ victoryexample/precache-manifest.9d88f25696b0e72bee61bb043e942885.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8c19bd41da7b542605d4d581bff763",
+    "url": "/victoryexample/index.html"
+  },
+  {
+    "revision": "a0feae8ef5adac73e8d4",
+    "url": "/victoryexample/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f61bac8e698198d83f9d",
+    "url": "/victoryexample/static/css/main.e486aef1.chunk.css"
+  },
+  {
+    "revision": "a0feae8ef5adac73e8d4",
+    "url": "/victoryexample/static/js/2.5d692e05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/victoryexample/static/js/2.5d692e05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f61bac8e698198d83f9d",
+    "url": "/victoryexample/static/js/main.1c83a991.chunk.js"
+  },
+  {
+    "revision": "96cf97f6a5f44647ba8c",
+    "url": "/victoryexample/static/js/runtime-main.313a0add.js"
+  },
+  {
+    "revision": "0824148b80aae00c252eb17fbfad8133",
+    "url": "/victoryexample/static/media/heading-arrow.0824148b.svg"
+  }
+]);
